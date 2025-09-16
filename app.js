a470d748fcd90d40fa9abccbdfd942a57460b0e3
EDUARDO FEITOSA BATISTA <25132675@CFP127ALUNO.LOCAL>
2025-09-16 13:58:32 -0300

    Evento ao clicar no Enter e abrir imagem individualmente Finalizado

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const main = document.querySelector('main')
 const btnPesquisar = document.getElementById('btn-pesquisa')
 const container = document.getElementById('container')
+const inputRaca = document.getElementById('input-pesquisa')
 
 async function buscarImagens (raca) {
     const url = `https://dog.ceo/api/breed/${raca}/images`
@@ -20,12 +21,23 @@ function exibirCachorro(cachorro){
     container.appendChild(img)
     container.classList.add('container')
     main.appendChild(container)
+
+    img.addEventListener('click', function(){
+        window.open(cachorro)
+    })
     
 }
 
+inputRaca.addEventListener('keydown', async(evento) => {
+    if(evento.key === 'Enter' || evento.keyCode === 13){
+        const imagens = await buscarImagens(inputRaca.value)
+        container.textContent = ''
+        imagens.forEach(img => exibirCachorro(img))
+    }
+})
+
 btnPesquisar.addEventListener('click', async() => {
-    const inputRaca = document.getElementById('input-pesquisa').value
-    const imagens = await buscarImagens(inputRaca)
+    const imagens = await buscarImagens(inputRaca.value)
     container.textContent = ''
     imagens.forEach(img => exibirCachorro(img))
 
